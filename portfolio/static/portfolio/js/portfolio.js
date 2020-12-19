695c4e0588ac4a4857b4f63facc050f23838ed38
sin-of-sloth <arjunlal0709@gmail.com>
2020-12-19 15:24:59 +0530

    cherry blossoms and stars

--- portfolio/static/portfolio/js/portfolio.js
+++ portfolio/static/portfolio/js/portfolio.js
@@ -1,7 +1,33 @@
+window.addEventListener('load', function () {
+    document.getElementById('theme-toggle').checked = false;
+    if(document.getElementById('theme').className == 'dark') {
+        document.getElementById('blossom_container').style.display = 'block';
+    }
+
+    for (var i = 0; i < 300; i++) {
+        var star = '<div class="star" style="animation: twinkle '+((Math.random()*5) + 5)+'s linear '+((Math.random()*5) + 5)+'s infinite; top: '+Math.random()*$(window).height()+'px; left: '+Math.random()*$(window).width()+'px;"></div>';
+        $('body').append(star);
+    }
+})
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems, {});
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems, {});
+  });
+
 function toggler() {
-    if (document.getElementById('theme') == "{% static 'portfolio/css/dark.css' %}") {
-        document.getElementById('theme').href = "{% static 'portfolio/css/dark.css' %}";
+    if (document.getElementById('theme').className == "dark") {
+        document.getElementById('theme').href = light_theme;
+        document.getElementById('theme').className = "light";
+        document.getElementById('blossom_container').style.visibility = 'visible';
     } else {
-        document.getElementById('theme').href = "{% static 'portfolio/css/light.css' %}"
+        document.getElementById('theme').href = dark_theme;
+        document.getElementById('theme').className = "dark";
+        document.getElementById('blossom_container').style.visibility = 'hidden';
     }
 }
--- portfolio/views.py
+++ portfolio/views.py
@@ -3,6 +3,4 @@ from .models import Project
 
 def home(request):
     projects = Project.objects.all()
-    if(request.COOKIES.get('theme', 'NULL') == 'NULL'):
-        request.COOKIES['theme'] = 'dark'
     return render(request, 'portfolio/home.html', {'projects':projects})
