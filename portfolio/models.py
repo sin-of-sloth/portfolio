ccf0e6bc69ed66d30f03307d6bbdb6f305be6681
sin-of-sloth <arjunlal0709@gmail.com>
2020-12-24 17:50:34 +0530

    project rows and order

--- portfolio/models.py
+++ portfolio/models.py
@@ -5,6 +5,7 @@ class Project(models.Model):
     description = models.CharField(max_length=250)
     image = models.ImageField(upload_to='portfolio/images/')
     url = models.URLField(blank=True)
+    order = models.IntegerField(default=99)
 
     def __str__(self):
         return str(self.id) + '- ' + self.title
--- portfolio/views.py
+++ portfolio/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Project
 
 def home(request):
-    projects = Project.objects.all()
+    projects = Project.objects.all().order_by('order')
     response = render(request, 'portfolio/home.html', {'projects':projects})
     if(request.COOKIES.get('theme', '') == ''):
         response.set_cookie('theme', 'light')
